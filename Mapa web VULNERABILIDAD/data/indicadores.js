90c2f9d6b729897e32b01dfc2c8af51ebb02fdd3
Cristian Holguin <crislholguin@gmail.com>
2026-04-01 15:50:53 -0300

    cargar algunas descripciones

--- Mapa web VULNERABILIDAD/data/indicadores.js
+++ Mapa web VULNERABILIDAD/data/indicadores.js
@@ -4,8 +4,8 @@ var indicadores_ref = [
     "grupo_nom": "Acceso a servicios de educación y salud",
     "tipo": "Binario",
     "variables": {
-      "1": { "label": "Residen a más de 5 cuadras de los servicios educativos y de salud" },
-      "0": { "label": "Reside a menos de 5 cuadras de algún servicio educativo y/o de salud" }
+      "1": { "label": "Menor cobertura de servicios (más de 5 cuadras)", "desc": "Reside a más de cinco cuadras de los servicios de salud y educación." },
+      "0": { "label": "Mayor cobertura de servicios (5 cuadras o menos)", "desc": "Reside a menos de cinco cuadras de los servicios de salud y educación."}
     }
   },
   {
@@ -13,8 +13,8 @@ var indicadores_ref = [
     "grupo_nom": "Cercania a vías de acceso (ruta, avenida, cruce de calles)",
     "tipo": "Binario",
     "variables": {
-      "1": { "label": "Residen a más de 5 cuadras" },
-      "0": { "label": "Reside a 5 cuadras o menos" }
+      "1": { "label": "Lejos de las vías de acceso (más de 5 cuadras)", "desc": "Reside a más de cinco cuadras de las vías de acceso." },
+      "0": { "label": "Cerca de las vías de acceso (5 cuadras o menos)", "desc": "Reside a cinco cuadras o menos de las vías de acceso." }
     }
   },
   {
@@ -22,8 +22,8 @@ var indicadores_ref = [
     "grupo_nom": "Exposición a factores de riesgo ambiental",
     "tipo": "Binario",
     "variables": {
-      "1": { "label": "Está expuesto a factores de riesgo ambiental" },
-      "0": { "label": "No está expuesto a factores de riesgo ambiental" }
+      "1": { "label": "Zonas con alto riesgo ambiental", "desc": "Está expuesto a factores de riesgo ambiental." },
+      "0": { "label": "Zonas con bajo riesgo ambiental", "desc": "No está expuesto a factores de riesgo ambiental." }
     }
   },
   {
@@ -31,8 +31,8 @@ var indicadores_ref = [
     "grupo_nom": "Indicador NBI Hacinamiento",
     "tipo": "Binario",
     "variables": {
-      "1": { "label": "Con Hacinamiento" },
-      "0": { "label": "Sin Hacinamiento" }
+      "1": { "label": "Con Hacinamiento", "desc": "Reside en un hogar con más de tres personas por cuarto." },
+      "0": { "label": "Sin Hacinamiento", "desc": "Reside en un hogar con tres personas o menos por cuarto." }
     }
   },
   {
@@ -40,8 +40,8 @@ var indicadores_ref = [
     "grupo_nom": "Servicios e infraestructura pública eficiente",
     "tipo": "Binario",
     "variables": {
-      "1": { "label": "Servicios e infraestructura pública ineficientes" },
-      "0": { "label": "Servicios e infraestructura pública eficientes" }
+      "1": { "label": "Servicios públicos ineficientes", "desc": "Los servicios públicos no están funcionando de manera eficiente." },
+      "0": { "label": "Servicios públicos eficientes", "desc": "Los servicios públicos están funcionando de manera eficiente." }
     }
   },
   {
@@ -49,8 +49,8 @@ var indicadores_ref = [
     "grupo_nom": "Tipo de trabajo",
     "tipo": "Binario",
     "variables": {
-      "1": { "label": "Informal-precario" },
-      "0": { "label": "Trabajo formal-estable" }
+      "1": { "label": "Informal-precario", "desc": "Cuenta con un trabajo informal o precario." },
+      "0": { "label": "Trabajo formal-estable", "desc": "Cuenta con un trabajo formal y estable." }
     }
   },
   {
@@ -58,11 +58,11 @@ var indicadores_ref = [
     "grupo_nom": "Calidad de los materiales (resistencia, durabilidad y aislamiento)",
     "tipo": "Categorizado",
     "variables": {
-      "4": { "label": "Calidad 4" },
-      "3": { "label": "Calidad 3" },
-      "2": { "label": "Calidad 2" },
-      "1": { "label": "Calidad 1" },
-      "9": { "label": "Sin datos" }
+      "4": { "label": "Calidad 4", "desc": "a vivienda presenta materiales no resistentes al menos en uno de los componentes, pero no en todos" },
+      "3": { "label": "Calidad 3", "desc": "La vivienda presenta materiales resistentes en todos los componentes, pero le faltan elementos de aislación o terminación en todos sus componentes, presenta techos de chapa de metal o fibrocemento u otros sin cielorraso, o paredes de chapa de metal o fibrocemento." },
+      "2": { "label": "Calidad 2", "desc": "La vivienda presenta materiales resistentes en todos los componentes, pero le faltan elementos de aislación o terminación al menos en uno de sus componentes, aunque no en todos." },
+      "1": { "label": "Calidad 1", "desc": "La vivienda presenta materiales resistentes en todos los componentes e incorpora todos los elementos de aislación y terminación." },
+      "9": { "label": "Sin datos", "desc": "No se dispone de información sobre la calidad de los materiales de la vivienda." }
     }
   },
   {
@@ -103,20 +103,20 @@ var indicadores_ref = [
       "7": { "label": "Inseguridad alimentaria, Línea de Pobreza y NBI" }
     }
   },
-  {
-    "cod_grupo": "205",
-    "grupo_nom": "Indice de Riesgo Infantil",
-    "tipo": "Categorizado",
-    "variables": {
-      "0": { "label": "0" },
-      "1": { "label": "1" },
-      "2": { "label": "2" },
-      "3": { "label": "3" },
-      "4": { "label": "4" },
-      "5": { "label": "5" },
-      "6": { "label": "6" }
-    }
-  },
+  // {
+  //   "cod_grupo": "205",
+  //   "grupo_nom": "Indice de Riesgo Infantil",
+  //   "tipo": "Categorizado",
+  //   "variables": {
+  //     "0": { "label": "0" },
+  //     "1": { "label": "1" },
+  //     "2": { "label": "2" },
+  //     "3": { "label": "3" },
+  //     "4": { "label": "4" },
+  //     "5": { "label": "5" },
+  //     "6": { "label": "6" }
+  //   }
+  // },
   {
     "cod_grupo": "206",
     "grupo_nom": "Indice de Riesgo Infantil (categorizado)",
